6ebdfd93d97105e7d860856ae0273e3ff9f8489c
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2013-09-03 17:51:02 +0200

    Added some basic tests

---
+++ web/config_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestReadConfigFrom(t *testing.T) {
+	filename := "test_data/config_test.json"
+	config, err := ReadConfigFrom(filename)
+	if err != nil {
+		t.Fatalf("failed: %s", err)
+	}
+	if len(config) != 3 {
+		t.Fatalf("expected 3 elements, got %s", len(config))
+	}
+	if len(config["ajaxchat"]) != 4 {
+		t.Fatalf("expected 4 elements, got %s", len(config["ajaxchat"]))
+	}
+	if len(config["general"]) != 1 {
+		t.Fatalf("expected 1 elements, got %s", len(config["general"]))
+	}
+	if len(config["tumbl"]) != 4 {
+		t.Fatalf("expected 4 elements, got %s", len(config["tumbl"]))
+	}
+}
+
+func TestGet(t *testing.T) {
+	c := Config{
+		"S1": map[string]string{
+			"k11": "v11",
+		},
+		"S2": map[string]string{
+			"k21": "v21",
+		},
+	}
+	if c.Get("S1", "k11") != "v11" {
+		t.Fatalf("got the wrong key")
+	}
+	if c.Get("S2", "k21") != "v21" {
+		t.Fatalf("got the wrong key")
+	}
+	defer func() {
+		if r := recover(); r != nil {
+		} else {
+			t.Fatalf("did not raise!")
+		}
+	}()
+	c.Get("S2", "k11")
+}
---
+++ web/message_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+func TestParseData(t *testing.T) {
+	filename := "test_data/login_fragment.xml"
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Fatalf("failed: %s", err)
+	}
+	xml, err := ParseData(data)
+	if err != nil {
+		t.Fatalf(fmt.Sprintf("%s", err))
+	}
+	if len(xml.Infos) != 5 || len(xml.Users) != 3 || len(xml.Messages) != 2 {
+		t.Fatalf("wrong number of pieces in demarshaled structure")
+	}
+	pass := false
+	for _, tstInfo := range xml.Infos {
+		if tstInfo.Type == "channelID" &&
+			tstInfo.Value == "0" {
+			pass = true
+		}
+	}
+	if !pass {
+		t.Fatalf("loaded info with wrong parameters")
+	}
+	for _, tstUser := range xml.Users {
+		if tstUser.Nick != "Newfag_tapirc" ||
+			tstUser.UserID != "471536092" ||
+			tstUser.UserRole != "0" ||
+			tstUser.ChannelID != "0" {
+			pass = true
+		}
+	}
+	if !pass {
+		t.Fatalf("loaded user with wrong parameters")
+	}
+
+	for _, tstMessage := range xml.Messages {
+		if tstMessage.Id != "212117" ||
+			tstMessage.DateTime != "Tue, 03 Sep 2013 01:02:51 +0200" ||
+			tstMessage.UserID != "471536092" ||
+			tstMessage.UserRole != "0" ||
+			tstMessage.ChannelID != "0" {
+			pass = true
+		}
+	}
+	if !pass {
+		t.Fatalf("loaded message with wrong parameters")
+	}
+}
